c5f250ae5292b2d9457d413cdb07382e6c1dd45e
Eugene Pankov <john.pankov@gmail.com>
2021-05-15 17:23:10 +0200

    PoC Putty private key parsing

--- test/putty.js
+++ test/putty.js
@@ -7,25 +7,26 @@ var sshpk = require('../lib/index');
 
 var testDir = path.join(__dirname, 'assets');
 
-var PUTTY_DSA, PUTTY_DSA_PUB, PUTTY_RSA, PUTTY_DSA_SSH, PUTTY_DSA_LONG;
+var PUTTY_DSA, PUTTY_DSA_PUB, PUTTY_RSA, PUTTY_RSA_SSH, PUTTY_DSA_SSH, PUTTY_DSA_LONG;
 
 test('setup', function (t) {
 	PUTTY_DSA = fs.readFileSync(path.join(testDir, 'dsa.ppk'));
 	PUTTY_DSA_PUB = fs.readFileSync(path.join(testDir, 'dsa-pub.ppk'));
 	PUTTY_RSA = fs.readFileSync(path.join(testDir, 'rsa.ppk'));
-	PUTTY_DSA_SSH = fs.readFileSync(path.join(testDir, 'dsa-ppk.pub'));
+	PUTTY_RSA_SSH = fs.readFileSync(path.join(testDir, 'rsa-ppk'));
+	PUTTY_DSA_SSH = fs.readFileSync(path.join(testDir, 'dsa-ppk'));
 	PUTTY_DSA_LONG = fs.readFileSync(path.join(testDir, 'dsa-pub-err.ppk'));
 	t.end();
 });
 
 test('parse DSA ppk file', function (t) {
-	var k = sshpk.parseKey(PUTTY_DSA, 'putty');
+	var k = sshpk.parsePrivateKey(PUTTY_DSA, 'putty', { passphrase: 'foobar' });
 	t.strictEqual(k.type, 'dsa');
 	t.strictEqual(k.fingerprint('sha256').toString(),
-	    'SHA256:wWAQNw7Px2Hymk7kimFL35jzdLqRt9V9hB5RH20YX8s');
-	t.strictEqual(k.comment, 'dsa-key-20170331');
-	var pub = k.toString('ssh');
-	t.strictEqual(pub, PUTTY_DSA_SSH.toString('ascii').trim());
+	    'SHA256:l95D4D6waUPH49RvkOLHWzwGSNKqg3GZx5f28UdcmDo');
+	t.strictEqual(k.comment, 'dsa-key-20210515');
+	var priv = k.toString('pem').trim();
+	t.strictEqual(priv, PUTTY_DSA_SSH.toString('ascii').trim());
 	t.end();
 });
 
@@ -33,17 +34,20 @@ test('parse DSA ppk file pub-only truncated', function (t) {
 	var k = sshpk.parseKey(PUTTY_DSA_PUB, 'putty');
 	t.strictEqual(k.type, 'dsa');
 	t.strictEqual(k.fingerprint('sha256').toString(),
-	    'SHA256:wWAQNw7Px2Hymk7kimFL35jzdLqRt9V9hB5RH20YX8s');
-	t.strictEqual(k.comment, 'dsa-key-20170331');
+	    'SHA256:l95D4D6waUPH49RvkOLHWzwGSNKqg3GZx5f28UdcmDo');
+	t.strictEqual(k.comment, 'dsa-key-20210515');
 	t.end();
 });
 
 test('parse RSA ppk file', function (t) {
-	var k = sshpk.parseKey(PUTTY_RSA, 'putty');
+	var k = sshpk.parsePrivateKey(PUTTY_RSA, 'putty', { passphrase: 'foobar' });
 	t.strictEqual(k.type, 'rsa');
 	t.strictEqual(k.fingerprint('sha256').toString(),
-	    'SHA256:WqDr0IdKUPtbitHUtWGGsA/xV/JhxPbVJG+E0SLWEig');
-	t.strictEqual(k.comment, 'rsa-key-20170331');
+	    'SHA256:DavVLu91hTy2jkSMKbb6maid8kpoCOP5YkB4Y3UX+dQ');
+	t.strictEqual(k.comment, 'rsa-key-20210515');
+
+	var priv = k.toString('pem').trim();
+	t.strictEqual(priv, PUTTY_RSA_SSH.toString('ascii').trim());
 	t.end();
 });
 
@@ -63,13 +67,14 @@ test('parse garbage as putty', function (t) {
 });
 
 test('parse RSA ppk file with auto', function (t) {
-	var k = sshpk.parseKey(PUTTY_RSA, 'auto');
+	var k = sshpk.parseKey(PUTTY_RSA, 'auto', { passphrase: 'foobar' });
 	t.strictEqual(k.type, 'rsa');
 	t.end();
 });
 
 test('generate dsa', function (t) {
 	var k = sshpk.parseKey(PUTTY_DSA_SSH);
+	k.comment = 'dsa-key-20210515'
 	var ppk = k.toString('putty');
 	t.strictEqual(ppk, PUTTY_DSA_PUB.toString('ascii'));
 	t.end();
